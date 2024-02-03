ee86aff9ef007acf59060038de5b54db5ba70d21
fahadkabali <kabalifahad@gmail.com>
2024-02-03 08:57:29 +0300

    added readME.md and gitignore file

--- wallet.js
+++ wallet.js
@@ -39,3 +39,54 @@ txb.sign(0, keyPair)
 const finalTxHex = txb.build().toHex()
 
 console.log(`Final Transaction Hex: ${finalTxHex}`)
+
+
+
+// const bitcoin = require('bitcoinjs-lib')
+// const ECPairFactory = require('ecpair').default
+// const ecc = require('tiny-secp256k1')
+// const ECPair = ECPairFactory(ecc)
+// const network = bitcoin.networks.testnet
+// let keyPair;
+
+
+// function generate_address() {
+//     const ECPair = ECP.ECPairFactory(ecc);
+//     const network = bitcoin.networks.testnet
+//     const keypair = ECPair.makeRandom({ network })
+//     const pubkey = keypair.publicKey
+//     const addressObject = bitcoin.payments.p2pkh({ pubkey, network })
+    
+//     const public_Key = keypair.publicKey.toString('hex');
+//     const private_Key = keypair.privateKey.toString('hex');
+
+//     console.log('address:',addressObject.address)
+//     console.log('keypair-private:',private_Key)
+//     console.log('keypair-public:',public_Key)
+// }
+
+// function initiate_transaction(private_key) {
+//     const ECPair = ECP.ECPairFactory(ecc);
+//     const network = bitcoin.networks.testnet;
+//     var privateKey = Buffer.from(private_key, 'hex');
+//     var keyPair = ECPair.fromPrivateKey(privateKey, { network: network });
+
+//     const outputNumber = 0;
+//     const txid = '9eac156e418ed1ca5ec4dbb539bc750a73a075dcb4a286bfc8e40b1cef40607d';
+//     const amount = 0.00090502;
+
+//     const psbt = new bitcoin.Psbt({network: network});
+//     const minerFee = 10000;
+//     const destinationAddress = 'mv4rnyY3Su5gjcDNzbMLKBQkBicCtHUtFB';
+//     const outputAmount = amount*1e8 - minerFee;
+//     const fullRawTransactionHex = '02000000000101c2add7b5dbe86fd8dd4fa8c420676c507bfa8a67b8423659209ade2b30140f170000000000fdffffff0286610100000000001976a9143d7c73abba50011192a02a618a46331d9288132388ac0ad04b2f000000001976a91484b860e19d5cd588a411fba64cda6909e97fb7f488ac0247304402200b19e0aba6e2e76c1de10ed0ce98198657b9caae984d96bf9ab847cd705d6e02022020dc0681db51c12aa83390672be4c716bfed7127a26778a70a838a537314c942012102b7194da20e99fb57a28f3affa5fd2f9c8b0ece1f5220aac040bb7696f8afb7955c4d2700'
+
+//     psbt.addInput({hash: txid, index: outputNumber, nonWitnessUtxo: Buffer.from(fullRawTransactionHex, 'hex')});
+//     psbt.addOutput({address: destinationAddress, value: outputAmount});
+//     psbt.signInput(outputNumber, keyPair);
+//     psbt.finalizeInput(0);
+//     const rawTransaction = psbt.extractTransaction().toHex();
+
+//     console.log(rawTransaction);
+     
+//     }
